b6370eba54991ee89e533e2853526411b51365d0
ChrisplayzYT/BLOX <191655784+ChrisplayzWii@users.noreply.github.com>
2025-05-05 18:27:00 -0500

    1

---
+++ js/regions.js
@@ -0,0 +1,13 @@
+function chooseUS() {
+// United States/US titles!
+parent.location.href="US.html";
+}
+
+function chooseUK() {
+// United Kingdom/UK titles!
+
+}
+function chooseJA() {
+// Japan titles!
+
+}
